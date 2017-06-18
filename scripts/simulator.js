d22ce8099b46d1b3113f02d5458dbef602de3e2c
Neil Ongkingco <neil.ongkingco@gmail.com>
2017-06-18 15:58:07 +0800

    Animated UI test

--- scripts/simulator.js
+++ scripts/simulator.js
@@ -7,32 +7,43 @@ function main() {
 
 $(document).ready(main);
 
+
+//Test code
+var uiTestInterval = null;
 function test() {
-  var newIds = ["blue1", "blue2", "blue3", "blue4"];
+  var blueIds = ["blue1", "blue2", "blue3", "blue4"];
   var components = ["head", "right_arm", "right_torso", "centre_torso", "left_arm", "left_torso", "right_leg", "left_leg"];
 
-  $.each(newIds, (index, newId) => {
+  $.each(blueIds, (index, newId) => {
     addMechPanel(newId, "#blueMechs");
-    for (var idx in components) {
-      setPaperDollArmor(newId, components[idx], 1 - (idx / components.length));
-      setPaperDollStructure(newId, components[idx], idx/components.length);
-    }
-    setHeatbarValue(newId, Math.random());
-    // addPaperDoll(newId, "#blueMechs");
-    // for (var idx in components) {
-    //   setPaperDollArmor(newId, components[idx], 1 - (idx / components.length));
-    //   setPaperDollStructure(newId, components[idx], idx/components.length);
-    // }
-    // addHeatbar(newId, "#blueMechs");
-    // setHeatbarValue(newId, Math.random());
   });
+  testUI(blueIds);
 
-  newIds = ["red1", "red2", "red3"];
-  $.each(newIds, (index, newId) => {
+  var redIds = ["red1", "red2", "red3"];
+  $.each(redIds, (index, newId) => {
     addMechPanel(newId, "#redMechs");
+  });
+  testUI(redIds);
+
+  $("#testUI").click(() => {
+    if (uiTestInterval == null) {
+      uiTestInterval = window.setInterval(() => {
+        testUI(blueIds);
+        testUI(redIds);
+      }, 100);
+    } else {
+      window.clearInterval(uiTestInterval);
+      uiTestInterval = null;
+    }
+  })
+}
+
+function testUI(mechIds) {
+  var components = ["head", "right_arm", "right_torso", "centre_torso", "left_arm", "left_torso", "right_leg", "left_leg"];
+  $.each(mechIds, (index, newId) => {
     for (var idx in components) {
-      setPaperDollArmor(newId, components[idx], 1 - (idx / components.length));
-      setPaperDollStructure(newId, components[idx], idx/components.length);
+      setPaperDollArmor(newId, components[idx], Math.random());
+      setPaperDollStructure(newId, components[idx], Math.random());
     }
     setHeatbarValue(newId, Math.random());
   });
