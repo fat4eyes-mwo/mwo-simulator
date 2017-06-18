c7baebc7cd1c31b80dab12e9cdba5992deafb4dd
Neil Ongkingco <neil.ongkingco@gmail.com>
2017-06-18 19:19:49 +0800

    Weapon UI framework and test.

--- scripts/simulator-test.js
+++ scripts/simulator-test.js
@@ -3,20 +3,51 @@
 //Test code
 var uiTestInterval = null;
 var testIntervalLength = 100;
+var mechIdWeaponCount = []; //number of weapons set for a given mechid
+
 function test() {
-  var blueIds = ["blue1", "blue2", "blue3", "blue4"];
+  var blueIds = ["blue1", "blue2", "blue3", "blue4", "blue5", "blue6", "blue7", "blue8", "blue9", "blue10", "blue11", "blue12"];
+  var redIds = ["red1", "red2", "red3","red4", "red5", "red6", "red7", "red8", "red9", "red10", "red11", "red12",];
   var components = ["head", "right_arm", "right_torso", "centre_torso", "left_arm", "left_torso", "right_leg", "left_leg"];
+  var testWeapons = [
+    [
+      {name : "SMALL PULSE LASER", location : "left_arm", ammo : "-1", state : "Active"},
+      {name : "SMALL PULSE LASER", location : "left_arm", ammo : "-1", state : "Active"},
+      {name : "SMALL PULSE LASER", location : "left_arm", ammo : "-1", state : "Active"},
+      {name : "SMALL PULSE LASER", location : "left_arm", ammo : "-1", state : "Active"},
+      {name : "SMALL PULSE LASER", location : "left_arm", ammo : "-1", state : "Active"},
+      {name : "SMALL PULSE LASER", location : "left_arm", ammo : "-1", state : "Active"},
+      {name : "SMALL PULSE LASER", location : "right_arm", ammo : "-1", state : "Active"},
+      {name : "SMALL PULSE LASER", location : "right_arm", ammo : "-1", state : "Active"},
+      {name : "SMALL PULSE LASER", location : "right_arm", ammo : "-1", state : "Active"},
+      {name : "SMALL PULSE LASER", location : "right_arm", ammo : "-1", state : "Active"},
+      {name : "SMALL PULSE LASER", location : "right_arm", ammo : "-1", state : "Active"},
+      {name : "SMALL PULSE LASER", location : "right_arm", ammo : "-1", state : "Active"}
+    ],
+    [ {name : "MEDIUM LASER", location : "centre_torso", ammo : "-1", state: "Active"},
+      {name : "MEDIUM LASER", location : "left_torso", ammo : "-1", state: "Active"},
+      {name : "SRM6", location : "left_arm", ammo : "200"}
+    ],
+    [ {name : "ER LARGE LASER", location : "left_torso", ammo : "-1", state: "Active"},
+      {name : "ER LARGE LASER", location : "right_torso", ammo : "-1", state: "Active"},
+      {name : "AC 10", location : "right_torso", ammo : "80", state: "Active"},
+      {name : "AC 5", location : "left_torso", ammo : "120", state: "Active"},
+    ]
+  ];
 
-  $.each(blueIds, (index, newId) => {
-    addMechPanel(newId, "#blueMechs");
+  $.each(blueIds, (index, mechId) => {
+    var testWeaponList = testWeapons[Math.floor(testWeapons.length * Math.random())];
+    addMechPanel(mechId, testWeaponList, "#blueMechs");
+    mechIdWeaponCount[mechId] = testWeaponList.length;
   });
-  testUI(blueIds);
+  // testUI(blueIds);
 
-  var redIds = ["red1", "red2", "red3"];
-  $.each(redIds, (index, newId) => {
-    addMechPanel(newId, "#redMechs");
+  $.each(redIds, (index, mechId) => {
+    var testWeaponList = testWeapons[Math.floor(testWeapons.length * Math.random())];
+    addMechPanel(mechId, testWeaponList, "#redMechs");
+    mechIdWeaponCount[mechId] = testWeaponList.length;
   });
-  testUI(redIds);
+  // testUI(redIds);
 
   $("#testUI").click(() => {
     if (uiTestInterval == null) {
@@ -33,11 +64,17 @@ function test() {
 
 function testUI(mechIds) {
   var components = ["head", "right_arm", "right_torso", "centre_torso", "left_arm", "left_torso", "right_leg", "left_leg"];
-  $.each(mechIds, (index, newId) => {
+  var weaponStates = ["Active", "Firing", "Disabled"];
+  $.each(mechIds, (index, mechId) => {
     for (var idx in components) {
-      setPaperDollArmor(newId, components[idx], Math.random());
-      setPaperDollStructure(newId, components[idx], Math.random());
+      setPaperDollArmor(mechId, components[idx], Math.random());
+      setPaperDollStructure(mechId, components[idx], Math.random());
     }
-    setHeatbarValue(newId, Math.random());
+    setHeatbarValue(mechId, Math.random());
+    for (var i = 0; i < mechIdWeaponCount[mechId]; i++) {
+      setWeaponCooldown(mechId, i, Math.random());
+      setWeaponAmmo(mechId, i, Math.random() > 0.2 ? Math.floor(Math.random() * 100) : -1);
+      setWeaponState(mechId, i, weaponStates[Math.floor(weaponStates.length * Math.random())]);
+    }
   });
 }
--- scripts/simulator-view.js
+++ scripts/simulator-view.js
@@ -18,7 +18,8 @@ var damageGradient = [
 
 //gets the damage color for a given percentage of damage
 function damageColor(percent) {
-  var damageIdx = binarySearchClosest(damageGradient, percent, (key, colorValue) => {
+  var damageIdx = binarySearchClosest(
+          damageGradient, percent, (key, colorValue) => {
     return key - colorValue.value;
   });
   if (damageIdx == -1) {
@@ -26,7 +27,6 @@ function damageColor(percent) {
   }
   return damageGradient[damageIdx].RGB;
 }
-
 
 //Add a paper doll with the given mechId to the element with the id
 //paperDollContainer uses the template paperDoll-template from the main HTML file
@@ -45,11 +45,20 @@ function addPaperDoll(mechId, paperDollContainer) {
 //Percent values from 0 to 1
 function setPaperDollArmor(mechId, location, percent) {
   var color = damageColor(percent);
-  $("#" + paperDollId(mechId) + "> [data-location='" + location + "']").css('border-color', "#" + color.toString(16));
+  $("#" + paperDollId(mechId) + "> [data-location='" + location + "']")
+    .css('border-color', "#" + color.toString(16));
 }
 function setPaperDollStructure(mechId, location, percent) {
   var color = damageColor(percent);
-  $("#" + paperDollId(mechId) + "> [data-location='" + location + "']").css('background-color', "#" + color.toString(16));
+  $("#" + paperDollId(mechId) + "> [data-location='" + location + "']")
+    .css('background-color', "#" + color.toString(16));
+}
+function setMaxArmorAndStructure(mechId) {
+  var components = ["head", "right_arm", "right_torso", "centre_torso", "left_arm", "left_torso", "right_leg", "left_leg"];
+  for (var idx in components) {
+    setPaperDollArmor(mechId, components[idx], 1);
+    setPaperDollStructure(mechId, components[idx], 1);
+  }
 }
 
 //Heatbar UI functions
@@ -64,25 +73,75 @@ function addHeatbar(mechId, heatbarContainer) {
   .removeClass("template")
   .appendTo(heatbarContainer);
 }
-
 //Sets the heatbar value for a given mech id to a specified percentage. Value of
 //percent is 0 to 1
 function setHeatbarValue(mechId, percent) {
   var invPercent = 1 - percent;
-  $("#" + heatbarId(mechId) + " > [class=heatbar]").height( (100 * invPercent) + "%");
+  $("#" + heatbarId(mechId) + " > [class=heatbar]")
+    .height( (100 * invPercent) + "%");
 }
 
-function onMouseOverPaperDoll() {
-  var mechId = $(this).parent().data("mech-id");
-  var location = $(this).data('location');
-  console.log (mechId + " " + location);
+//Adds a list of weaponRows to the given weaponPanel. Prototype of weaponList is
+//[(name, location, ammo, state)] where state is Active, Firing or Disabled
+function weaponRowId(mechId, idx) {
+  return mechId + "-" + idx + "-weaponrow";
+}
+function weaponCooldownBarId(mechId, idx) {
+  return weaponRowId(mechId, idx) + "-weaponCooldownBar";
+}
+function weaponAmmoId(mechId, idx) {
+  return weaponRowId(mechId, idx) + "-weaponAmmo";
+}
+var weaponLocAbbr = {
+  "head" : "H",
+  "left_arm" : "LA",
+  "left_torso" : "LT",
+  "centre_torso" : "CT",
+  "right_torso" : "RT",
+  "right_arm" : "RA",
+  "left_leg" : "LL",
+  "right_leg" : "RL"
+}
+function addWeaponPanel(mechId, weaponList, weaponPanel) {
+  for (var idx in weaponList) {
+    var weapon = weaponList[idx];
+    $("#weaponRow-template")
+      .clone(true)
+      .attr("id", weaponRowId(mechId, idx))
+      .attr("data-mech-id", mechId)
+      .attr("data-weapon-idx", idx)
+      .removeClass("template")
+      .appendTo(weaponPanel);
+    $("#" + weaponRowId(mechId, idx) + " .weaponName")
+      .attr("id", weaponRowId(mechId, idx) + "-weaponName")
+      .html(weapon.name);
+    $("#" + weaponRowId(mechId, idx) + " .weaponLocation")
+      .attr("id", weaponRowId(mechId, idx) + "-weaponLocation")
+      .html(weaponLocAbbr[weapon.location]);
+    $("#" + weaponRowId(mechId, idx) + " .weaponCooldownBar")
+      .attr("id", weaponCooldownBarId(mechId, idx));
+    $("#" + weaponRowId(mechId, idx) + " .weaponAmmo")
+      .attr("id", weaponAmmoId(mechId, idx)); //infinity symbol
+    setWeaponAmmo(mechId, idx, weapon.ammo);
+    setWeaponState(mechId, idx, weapon.state);
+  }
+}
+function setWeaponCooldown(mechId, weaponIdx, percent) {
+  $("#" + weaponCooldownBarId(mechId, weaponIdx)).width(100*percent + "%");
+}
+function setWeaponAmmo(mechId, weaponIdx, ammo) {
+  $("#" + weaponAmmoId(mechId, weaponIdx)).html(ammo != -1 ? ammo : "&#x221e;");
+}
+function setWeaponState(mechId, weaponIdx, state) {
+  $("#" + weaponRowId(mechId, weaponIdx)).removeClass("Active Firing Disabled");
+  $("#" + weaponRowId(mechId, weaponIdx)).addClass(state);
 }
 
 //adds a mech panel (which contains a paperDoll, a heatbar and a weaponPanel)
 function mechPanelId(mechId) {
   return mechId + "-mechPanel";
 }
-function addMechPanel(mechId, mechPanelList) {
+function addMechPanel(mechId, weaponList, mechPanelList) {
   $("#mechPanel-template")
     .clone(true)
     .attr("id", mechPanelId(mechId))
@@ -90,12 +149,13 @@ function addMechPanel(mechId, mechPanelList) {
     .removeClass("template")
     .appendTo(mechPanelList);
 
-  //a bit messy code to add paperDoll and heatbar. Try to see if this can be
-  //done inside a single search for the children of the mechPanel
+  //TODO: messy repetitive code to add paperDoll and heatbar. Try to see if this
+  //can be done inside a single search for the children of the mechPanel
   var paperDollContainerId = mechId + "-paperDollContainer";
   $("#" + mechPanelId(mechId) + " [class~='paperDollContainer']")
     .attr("id", paperDollContainerId);
   addPaperDoll(mechId, "#" + paperDollContainerId);
+  setMaxArmorAndStructure(mechId);
 
   var heatbarContainerId = mechId + "-heatbarContainer";
   $("#" + mechPanelId(mechId) + " [class~='heatbarContainer']")
@@ -105,11 +165,17 @@ function addMechPanel(mechId, mechPanelList) {
   var weaponPanelContainerId = mechId + "-weaponPanelContainer";
   $("#" + mechPanelId(mechId) + " [class~='weaponPanelContainer']")
     .attr("id", weaponPanelContainerId);
+  addWeaponPanel(mechId, weaponList, "#" + weaponPanelContainerId);
 }
-
 
 function initHandlers() {
   initPaperDollHandlers();
+}
+
+function onMouseOverPaperDoll() {
+  var mechId = $(this).parent().data("mech-id");
+  var location = $(this).data('location');
+  console.log (mechId + " " + location);
 }
 
 function initPaperDollHandlers() {
