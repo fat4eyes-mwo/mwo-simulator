176c7c380cd7e763224e90c9707ad327a86efe7d
Neil Ongkingco <neil.ongkingco@gmail.com>
2017-06-24 17:40:10 +0800

    Syntax error fixes

--- scripts/simulator-componenttarget.js
+++ scripts/simulator-componenttarget.js
@@ -3,7 +3,7 @@ var MechTargetComponent = MechTargetComponent || (function () {
 
   //These functions return which component of a mech should be targetted
   //function(sourceMech, targetMech) -> MechModel.Component
-  var aimForCenterTorso(sourceMech, targetMech) {
+  var aimForCenterTorso = function(sourceMech, targetMech) {
     return MechModel.Component.CENTRE_TORSO;
   }
 
--- scripts/simulator-mechtarget.js
+++ scripts/simulator-mechtarget.js
@@ -3,7 +3,7 @@ var MechTargetMech = MechTargetMech || (function () {
 
   //These functions return which enemy mech to target
   //function(MechModel.Mech, [MechModel.Mech])-> MechModel.Mech
-  var targetMechsInOrder(mech, enemyMechList) {
+  var targetMechsInOrder = function (mech, enemyMechList) {
     for (let enemyMech of enemyMechList) {
       if (enemyMech.mechState.isAlive()) {
         return enemyMech;
