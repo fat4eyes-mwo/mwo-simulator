e77067d42e71bd733d4d45a964b5df3c435b6727
Neil Ongkingco <neil.ongkingco@gmail.com>
2017-07-02 14:18:32 +0800

    Sanitize params to server-side persistence.

--- php/simulator-persistence.php
+++ php/simulator-persistence.php
@@ -7,6 +7,7 @@
   const POST_STATE_FIELD = "state";
   const POST_STATEHASH_FIELD = "statehash";
   const GET_STATE_FIELD = "s";
+  const JSON_STRING_LIMIT = 100000;
 
   $currDir = getcwd();
   $dataDir = $currDir . "/data";
@@ -23,6 +24,11 @@
     $outputStateData = array(POST_STATE_FIELD => $stateData);
 
     $jsonStateData = json_encode($outputStateData);
+    if (strlen($jsonStateData) > JSON_STRING_LIMIT) {
+      http_response_code(500);
+      echo "State string too long (>" . JSON_STRING_LIMIT . " bytes)";
+      exit;
+    }
 
     $jsonHash = hash("sha256", $jsonStateData, false);
 
@@ -42,6 +48,13 @@
   //Get request to fetch state
   } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $getStateHash = $_GET[GET_STATE_FIELD];
+    $getStateHash = filter_var(trim(strval($getStateHash)), FILTER_SANITIZE_SPECIAL_CHARS);
+    if ($getStateHash === FALSE) {
+      http_response_code(400);
+      echo "Invalid hash";
+      exit;
+    }
+
     $filename = $dataDir . "/" . $getStateHash;
 
     $jsonStateData = file_get_contents($filename);
