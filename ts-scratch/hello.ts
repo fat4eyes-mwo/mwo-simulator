9093eeb010b95edd896697a1d1bdad5e8fd1a643
Neil Ongkingco <neil.ongkingco@gmail.com>
2017-07-27 10:13:10 +0800

    Typescript experiments dir.

---
+++ ts-scratch/hello.ts
@@ -0,0 +1,10 @@
+class Greeter {
+    constructor(public greeting: string) { }
+    greet() {
+        return "<h1>" + this.greeting + "</h1>";
+    }
+};
+
+var greeter = new Greeter("Hello, Ministry of Programming!");
+var str = greeter.greet();
+console.log(str);
